31c798d0984bb626f8cacd158c287fc0ace8baf1
turgibot <turgibot@gmail.com>
2022-07-18 13:06:21 +0300

    Add Entitites DTOs

---
+++ RestaurantListing/DTOs/LocationDTO.cs
@@ -0,0 +1,28 @@
+﻿using RestaurantListing.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantListing.Core.DTOs
+{
+    public class CreateLocationDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Country Name Is Too Long")]
+        public string Country { get; set; }
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "CIty Name Is Too Long")]
+        public string City { get; set; }
+        [Required]
+        public int RestaurantId { get; set; }
+
+    }
+
+    public class LocationDTO : CreateLocationDTO
+    {
+        public int Id { get; set; }
+        public RestaurantDTO Restaurant { get; set; }
+    }
+
+    public class UpdateLocationDTO : CreateLocationDTO { }
+}
+
+
---
+++ RestaurantListing/DTOs/RestaurantDTO.cs
@@ -0,0 +1,44 @@
+﻿using RestaurantListing.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantListing.DTOs
+{
+    public class BaseRestaurantDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Restaurant Name Is Too Long")]
+        public string Name { get; set; }
+        [Required]
+        public DateTime VisitedOn { get; set; }
+    }
+
+    public class CreateRestaurantDTO : BaseRestaurantDTO
+    {
+        [Required]
+        public CreateLocationDTO Location { get; set; }
+
+        public List<CreateDishDTO> Dishes { get; set; }
+    }
+
+    public class UpdateRestaurantDTO : BaseRestaurantDTO
+    {
+        [Required]
+        public UpdateLocationDTO Location { get; set; }
+
+        [Required]
+        public List<UpdateDishDTO> Dishes { get; set; }
+    }
+
+    public class RestaurantDTO : BaseRestaurantDTO
+    {
+        public int Id { get; set; }
+
+        //8 a Restaurant has a location
+        public LocationDTO Location { get; set; }
+
+        //9 A restaurant has many dishes
+        public List<DishDTO> Dishes { get; set; }
+    }
+}
