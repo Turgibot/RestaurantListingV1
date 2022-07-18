1bcd1cb8e23e1a9ddfd9c3f4e261bd62560ff3c3
turgibot <turgibot@gmail.com>
2022-07-18 12:04:39 +0300

    Add DishDTO

---
+++ RestaurantListing/DTOs/DishDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantListing.DTOs
+{
+    public class CreateDishDTO
+    {
+        [Required]
+        [StringLength(maximumLength:50, ErrorMessage = "Dish Name is too Long")]
+        public string Name { get; set; }
+        [Required]
+        [Range(1,5)]
+        public int Stars { get; set; }
+
+        public string Review { get; set; }
+        [Required]
+        public int RestaurantId { get; set; }
+       
+    }
+
+    public class DishDTO: CreateDishDTO
+    {
+        public int Id { get; set; }
+        //public RestaurantDTO Restaurant { get; set; }
+    }
+
+    public class UpdateDishDTO : CreateDishDTO { }
+
+}
